f8b5f85c55cb28ac27b3253f734654be900c195a
lgomez <leandro.gz73@gmail.com>
2013-11-28 18:32:44 -0300

    Add rails app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainer::Application.config.secret_key_base = 'bdc0e43788db8934eeb19b48993b096b69e63ebf96248bc9680af2cd252b9ef9115c7b9af08b8dd023a7469121023b0b087b69cb9f39eb428a1e537875a81bb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rainer::Application.config.session_store :cookie_store, key: '_rainer_session'
